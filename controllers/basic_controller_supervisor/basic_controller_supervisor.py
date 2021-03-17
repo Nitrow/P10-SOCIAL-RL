2238486bbc6719091c00bb13a09de3c736c39704
Mark Adamik <dorteel@gmail.com>
2021-03-17 17:42:33 +0100

    EDIT: Inverse Kinematics Working

--- controllers/basic_controller_supervisor/basic_controller_supervisor.py
+++ controllers/basic_controller_supervisor/basic_controller_supervisor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.8
 """
 
 
@@ -8,11 +8,30 @@
 
 
 from controller import Robot, Motor, Supervisor
-import sys
-#import ikpy
+
+import ikpy
+import torch
+from ikpy.chain import Chain
+import matplotlib.pyplot as plt
+import ikpy.utils.plot as plot_utils
 
 # create the Robot instance.
 #robot = Robot()
+
+
+
+my_chain = Chain.from_urdf_file("../../resources/robot.urdf")
+
+target_position = [ 0.1, -0.2, 0.1]
+target_joints = my_chain.inverse_kinematics(target_position)
+
+def set_joints(target, motors):
+    
+    target_joints = my_chain.inverse_kinematics(target)
+    print("Moving to position {}\t{}".format(target, target_joints))
+    for i in range(len(motors)):
+        motors[i].setPosition(target_joints[i+1])
+
 
 TIME_STEP = 32
 MAX_SPEED = 6.28
@@ -24,10 +43,6 @@ supervisor = Supervisor()
 robot_node = supervisor.getFromDef("UR3")
 conveyor_node = supervisor.getFromDef("conveyor")
 tv_node = supervisor.getFromDef("TV")
-
-if robot_node is None:
-    sys.stderr.write("No DEF MY_ROBOT node found in the current world file\n")
-    sys.exit(1)
     
 trans_field = robot_node.getField("translation")
 counter+=1
@@ -45,7 +60,7 @@ sensors = [0] * len(joint_names)
 for i in range(len(joint_names)):
     # Get motors
     motors[i] = supervisor.getDevice(joint_names[i])
-    motors[i].setPosition(0)
+    motors[i].setPosition(target_joints[i])
     #motors[i].setPosition(float('inf'))
     #motors[i].setVelocity(1 * MAX_SPEED)
     
@@ -53,15 +68,23 @@ for i in range(len(joint_names)):
     sensors[i] = supervisor.getDevice(joint_names[i]+'_sensor')
     sensors[i].enable(TIME_STEP)
 
+x = 0.3
+y=0.3
+target = [ x,x,x]
 
 while supervisor.step(TIME_STEP) != -1:
     counter += 1
-    print(counter)
+    set_joints(target, motors)
+    #print(counter)
+
+    y -= 0.001
+    target = [ x,y,x]
     if counter > max_iter:
         counter = 0
-        supervisor.simulationReset()
-        conveyor_node.restartController()
-        tv_node.restartController()
+        
+        #supervisor.simulationReset()
+        #conveyor_node.restartController()
+        #tv_node.restartController()
     # read sensors outputs
     # print("Sensor outputs:\n")
     # readings = ""
