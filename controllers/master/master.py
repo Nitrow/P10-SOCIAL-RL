6244db3f1476891ae325083bc0b2d8f35f97e933
Mark Adamik <dorteel@gmail.com>
2021-04-08 17:39:10 +0200

    Reset other env

--- controllers/master/master.py
+++ controllers/master/master.py
@@ -11,9 +11,11 @@ from P10_RL_env_v01.envs import P10RLEnv
 
 if __name__ == '__main__':
 	
-    env_id = 'SAC_P10_MarkEnv'	
-    #env = P10RLEnv()
-    env = P10_DRL_Mark_Env()
+    #env_id = 'SAC_P10_MarkEnv'
+    #env = P10_DRL_Mark_Env()	
+    env_id = 'SAC_P10'
+    env = P10RLEnv()
+
     agent = Agent(alpha=0.0003, beta=0.0003, reward_scale=2, env_id=env_id, 
                 input_dims=env.observation_space.shape, tau=0.005,
                 env=env, batch_size=256, layer1_size=256, layer2_size=256,
