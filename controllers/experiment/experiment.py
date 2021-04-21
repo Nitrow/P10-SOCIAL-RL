cbbd9080e3adeaa2a33136580fd5849a0381e7ca
Mark Adamik <dorteel@gmail.com>
2021-04-21 16:25:24 +0200

    EDIT: Clicking at clans (not fully working, still needs debugging)

--- controllers/experiment/experiment.py
+++ controllers/experiment/experiment.py
@@ -3,13 +3,15 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Camera, Supervisor, Display
+from controller import Robot, Camera, Supervisor, Display, MouseState, Mouse
 import numpy as np
 import cv2
 # create the Robot instance.
-robot = Robot()
+#robot = Robot()
+supervisor = Supervisor()
+robot = supervisor.getFromDef("UR3")
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+timestep = int(supervisor.getBasicTimeStep())
 cv2.startWindowThread()
 cv2.namedWindow("preview")
 padding = np.array([10, 10])
@@ -20,6 +22,9 @@ display = Display("display")
 
 width = camera.getWidth()
 height = camera.getHeight()
+mouse = Mouse()
+mouse.enable(timestep)
+mouse.enable3dPosition()
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -29,10 +34,10 @@ height = camera.getHeight()
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+
+def drawImage(camera):
     cameraData = camera.getImage()
     image = np.frombuffer(cameraData, np.uint8).reshape((camera.getHeight(), camera.getWidth(), 4))
-    
     for object in camera.getRecognitionObjects():
         size = np.array(object.get_size_on_image()) + padding
         start_point = np.array(object.get_position_on_image()) - (size / 2) 
@@ -43,21 +48,40 @@ while robot.step(timestep) != -1:
         #color = [0, 255, 0]
         color = ( int (color [ 0 ]), int (color [ 1 ]), int (color [ 2 ]))
         image = cv2.rectangle(image, tuple(start_point), tuple(end_point), tuple(color), thickness)        
-    #print(camera.getRecognitionObjects())
-    #print(cameraData)
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-    #ir = display.imageNew(image.tolist(), Display.RGB)#, width, height)
-    #display.imagePaste(ir, 20, 0)
-    #display.imageDelete(ir)
-    
     cv2.imshow("preview", image)
     cv2.waitKey(timestep)
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+       
+
+prevClick = False
+doubleClick = False
+selection = None
+prevSelection = None
+canSelection = None
+can_height = 0.85
+while supervisor.step(timestep) != -1:
+    event = mouse.getState()
+    click = event.left
+    #print(click)
+    if (click and not prevClick):
+        selection = supervisor.getSelected()
+        if selection == prevSelection: doubleClick = True
+        selectionName = selection.getDef()
+        
+        if ("CAN" in selectionName):
+            canSelection = selection.getField("translation")
+            
+        elif ("CRATE" in selectionName) and canSelection:
+            new_position = selection.getField("translation").getSFVec3f()
+            new_position[1] = can_height
+            if doubleClick:
+                canSelection.setSFVec3f(new_position)
+                canSelection = None
+    doubleClick = False
+    prevSelection = selection
+    prevClick = click
+    if prevSelection != prevSelection and selection != selection:
+        print("Current selection: {} \t Previous selection: {}".format(selection.getDef(),prevSelection.getDef()))
+    drawImage(camera)
+
 
 # Enter here exit cleanup code.
