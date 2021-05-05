c5252d0273f4c8decaff037f5ab9de40504fcdeb
Nitrow <printz.a91@gmail.com>
2021-05-05 12:17:44 +0200

    added orientation training

--- controllers/masterStable/masterStable.py
+++ controllers/masterStable/masterStable.py
@@ -2,9 +2,10 @@
 import gym
 import numpy as np
 #import P10_RL_env_v01
-import P10_DRL_Mark
+#import P10_DRL_Mark
 import torch as th
 from sb3_contrib import TQC
+from P10_DRL_Mark_SingleJoint.envs import P10_DRL_Mark_SingleJointEnv
 
 
 from stable_baselines3 import PPO
@@ -14,14 +15,14 @@ from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckA
 policy_kwargs = dict(activation_fn=th.nn.ReLU,
                      net_arch=[dict(pi=[256, 256], vf=[256, 256])])
 
-env = gym.make('P10-DRL-Mark-v0')
+env = P10_DRL_Mark_SingleJointEnv()
 
 # The noise objects for TD3
 n_actions = env.action_space.shape[-1]
 action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.001 * np.ones(n_actions))
 
 #model =  PPO("MlpPolicy", env, learning_rate=0.0003, verbose=2)  # , tensorboard_log="/home/asger/P10-XRL/controllers/masterStable/tensorboard")
-model = TQC("MlpPolicy", env, top_quantiles_to_drop_per_net=2, verbose=1)
+model = TQC("MlpPolicy", env, top_quantiles_to_drop_per_net=2, verbose=1, tensorboard_log="/home/asger/P10-XRL/controllers/masterStable/tensorboard")
 model.learn(total_timesteps=10000000, log_interval=1)
 model.save("ppo_p10")
 env = model.get_env()
