41c7c9f60212c890b18574fb384c5a9ae25e3d31
Mark Adamik <dorteel@gmail.com>
2021-05-18 16:40:55 +0200

    ADD: 42nd participant

--- controllers/experiment/experiment.py
+++ controllers/experiment/experiment.py
@@ -8,9 +8,9 @@ import os
 import math
 
 # easy or hard
-participantId = "41"
+participantId = "42"
 gameMode = 2
-condition = "all"
+condition = "control"
 filepath = "data/" + participantId + "-" + condition + "-" + str(gameMode) + ".txt"
 # user correct, user incorrect, robot correct, robot incorrect, miss
 experiment_conditions = {"control" : [False, False, False],
