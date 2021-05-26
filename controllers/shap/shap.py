cb944c9cfd7430eac4506d336e01131e62cf5c67
Nitrow <printz.a91@gmail.com>
2021-05-26 19:10:56 +0200

    shap

--- controllers/shap/shap.py
+++ controllers/shap/shap.py
@@ -6,12 +6,20 @@ from dueling_ddqn_agent import DuelingDDQNAgent
 from utils import plot_learning_curve, make_env
 import shap
 import torch as T
+#import sys
 
+
+#print(sys.version)
 
 from P10_DRL_D3QN.envs import P10_DRL_D3QNEnv
 
 if __name__ == '__main__':
-    feature_names=['Position\nin m', 'orientation\nin degree', 'color']
+    feature_names = ['can 1 x', 'can 1 y', 'can 1 z', 'can 1 rot x', 'can 1 roty', 'can 1 rotz', 'can 1 angle', 'can 1 color x', 'can 1 color y', 'can 1 color z',
+                     'can 2 x', 'can 2 y', 'can 2 z', 'can 2 rot x', 'can 2 roty', 'can 2 rotz', 'can 2 angle', 'can 2 color x', 'can 2 color y', 'can 2 color z',      
+                     'can 3 x', 'can 3 y', 'can 3 z', 'can 3 rot x', 'can 3 roty', 'can 3 rotz', 'can 3 angle', 'can 3 color x', 'can 3 color y', 'can 3 color z',      
+                     'can 4 x', 'can 4 y', 'can 4 z', 'can 4 rot x', 'can 4 roty', 'can 4 rotz', 'can 4 angle', 'can 4 color x', 'can 4 color y', 'can 4 color z',      
+                     'can 5 x', 'can 5 y', 'can 5 z', 'can 5 rot x', 'can 5 roty', 'can 5 rotz', 'can 5 angle', 'can 5 color x', 'can 5 color y', 'can 5 color z',      
+                     'can 6 x', 'can 6 y', 'can 6 z', 'can 6 rot x', 'can 6 roty', 'can 6 rotz', 'can 6 angle', 'can 6 color x', 'can 6 color y', 'can 6 color z']
 
     env = P10_DRL_D3QNEnv()
     best_score = -np.inf
